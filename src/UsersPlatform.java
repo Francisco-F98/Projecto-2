da869724ca35a7c0953d1b33bb02f17afe3aa901
EltonVieira98 <eltonviei1998@gmail.com>
2025-11-21 17:48:13 +0000

    Update UsersPlatform.java

--- src/UsersPlatform.java
+++ src/UsersPlatform.java
@@ -1,29 +1,33 @@
 public class UsersPlatform {
-    private User[]users;
+    private final User[]users;
     public UsersPlatform() {
         users = new User[5];
     }
     public void createUser(String username) {
         int idx=availableIdx();
         users[idx]=new User(username);
+        System.out.println(idx);
+
+
     }
     private int availableIdx(){
-        int emptyIdx=0;
-        int i=0;
-      do{
-          if(users[i] == null){
-              emptyIdx=i;
-          }
-            i++;
-      }while(users[i]!=null);
-          return emptyIdx;
+        int emptyIdx=-1;
+        for(int i=0;i<users.length&&emptyIdx==-1;i++){
+           if(users[i]==null){
+               emptyIdx = i;
+           }
+       }
+
+        return emptyIdx;
     }
     public boolean exists(String username) {
+        int i=0;
         boolean exist=false;
-        for(int i=0;i<users.length;i++){
-        if(users[i]!=null&&username.equals(users[i].getUsername())){
-        exist=true;
-        break;}
+        while(i<users.length&&users[i]!=null&& !exist){
+            if(users[i].getUsername().equals(username)){
+                exist=true;
+            }
+            i++;
         }
         return exist;
         }
