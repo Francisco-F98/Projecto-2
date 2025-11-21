7125e94732b50dd8b03fea9f6ac1fabbb5e6c231
EltonVieira98 <eltonviei1998@gmail.com>
2025-11-21 17:48:39 +0000

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,7 +1,10 @@
 public class User {
   private String username;
+  private UserCalender calender;
   public User(String username) {
       this.username=username;
+      this.calender=new UserCalender();
+
   }
   public String getUsername() {
       return username;
