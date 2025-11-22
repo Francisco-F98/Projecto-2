fa49e075820953f895f43f781972581ac4571f04
Francisco-F98 <poisepoise74@gmail.com>
2025-11-22 20:00:26 +0000

    Fiz a class dos users, podem vir a ser precisos mais metodos.
    Vou fazer a classe dos eventos

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,85 @@
 // Projecto 2 - Elton Vieira, Francisco Ferreira
-
 import java.util.Scanner;
+
+// classes a implementar: User, Evento, SystemManager,
+
+//Representa os utilizadores(nome, eventos, nr de eventos)
+public class User{
+
+    // max 200 events por user
+    private int max_events = 200;
+    // nome tem de ser unico
+    private String name;
+    // array dos eventos do user
+    private event[] events;
+    // nr de eventos associados ao user
+    private int event_count;
+
+
+    // constructor
+    public User(String name){
+        this.name = name;
+        this.events = new event[max_events];
+        this.event_count = 0;
+    }
+
+
+    //getters:
+    public String get_name(){
+        return name;
+    }
+    public Event[] get_Events() {
+        return events;
+    }
+    public int get_eventCount(){
+        return event_count;
+    }
+
+
+    // adiciona o proximo evento no calendario
+    public void add_event(Event eventname){
+        events[event_count]= eventname;
+        event_count ++;
+    }
+
+
+    // remove evento no calendario e substitui pelo ultimo elemento de events.
+    public void del_event(Event eventname){
+        for (int i = 0; i< event_count; i++) {
+            if (events[i].equals(eventname)) {         // usar .equals??
+                events[i] = events[event_count - 1];
+                event_count--;
+            }
+        }
+    }
+
+
+    // verificar conflitos de horario   // day: 1-5.
+    public boolean conflit(int day, int inicio, int fim){
+        for (int i=0; i< event_count; i++){
+            Event eventname = events[i];
+            // metodos definidos na classe Event
+            if (eventname.get_day() == day){
+                // existe conflito? ( há sobreposição? )
+                if ( (inicio < eventname.getEnd()) && (fim  > eventname.getStart()) ){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+}
+
+// Representa um evento
+public class Event{
+
+}
+
+
+
+
 public class Main {
     private static final String CREATE_CMD="create";
     private static final String SCHEDULE_CMD="schedule";
@@ -20,6 +99,8 @@ public class Main {
     private static final String CANCEL_ERROR="Event cancelled.";
     private static final String EXITED="Application exited.";
 
+
+    // não percebi?? criar um user fora da classe?
     private static void createUser(UsersPlatform user,String[] name) {
         String username=name[1];
         if(user.exists(username)){System.out.println(CREATE_ERROR);}
