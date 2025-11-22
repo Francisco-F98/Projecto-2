21ffe254f297bfc5580886eb583a3a106bfa9652
Francisco-F98 <poisepoise74@gmail.com>
2025-11-22 20:35:08 +0000

    Class users e evento feita

--- src/Main.java
+++ src/Main.java
@@ -61,7 +61,7 @@ public class User{
             // metodos definidos na classe Event
             if (eventname.get_day() == day){
                 // existe conflito? ( há sobreposição? )
-                if ( (inicio < eventname.getEnd()) && (fim  > eventname.getStart()) ){
+                if ( (inicio < eventname.get_end()) && (fim  > eventname.get_start()) ){
                     return true;
                 }
             }
@@ -75,7 +75,64 @@ public class User{
 // Representa um evento
 public class Event{
 
+    //max participantes por evento
+    private int max_part = 10;
+
+
+    private String name;
+    // dia do evento: 1-5
+    private int day;
+    //hora de inicio: 8-19
+    private int start;
+    //hora de fim: 9-20
+    private int end;
+    //array com os participantes no evento
+    private User[] participants;
+    // nr de participantes
+    private int participant_count;
+
+    // constructor
+    public Event(String name,int day,int start, int end, User[] parts, int part_count){
+
+        this.name = name;
+        this.day = day;
+        this.start = start;
+        this.end= end;
+
+        this.participants = new User[max_part];
+        this.participant_count = part_count;
+
+        for (int i = 0; i < part_count;i++){
+            this.participants[i]=  parts[i];
+        }
+    }
+
+    // getters:
+    public String get_name(){
+        return name;
+    }
+    public int get_day(){
+        return day;
+    }
+    public int get_start(){
+        return start;
+    }
+    public int get_end(){
+        return end;
+    }
+    public int get_participantCount(){
+        return participant_count;
+    }
+    public User get_proponente(){
+        return participants[0];
+    }
+    public User[] get_participants(){
+        return participants;
+    }
+
+
 }
+
 
 
 
