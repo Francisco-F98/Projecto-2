e36ee3ba7f33696741fd78e7252f8676d641b31b
EltonVieira98 <eltonviei1998@gmail.com>
2025-11-28 23:01:26 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
     private static final String SHOW_CMD = "show";
     private static final String TOP_CMD = "top";
     private static final String EXIT_CMD = "exit";
-    private static final String INVALID = "Invalid command";
+    private static final String INVALID_CMD = "Invalid command.";
     private static final String CREATE_USER = "User successfully created.";
     private static final String CREATE_ERROR = "User already registered.";
     private static final String SCHEDULE_CREATED = "Event successfully created.";
@@ -19,41 +19,26 @@ public class Main {
     private static final String SCHEDULE_ERROR_NOT = "Some user not registered.";
     private static final String SCHEDULE_ERROR_USER = "Some user not available.";
     private static final String SCHEDULE_ERROR_PROPOSER = "Proposer not available.";
-    private static final String CANCEL_SUCCESS = "Event successfully cancelled.";
-    private static final String CANCEL_ERROR = "Event cancelled.";
+    private static final String CANCEL_SUCCESS = "Event successfully canceled.";
     private static final String SHOW_ERROR = "User not registered.";
     private static final String EXITED = "Application exited.";
 
     // Criação da classe SystemManager  e leitura do Text file.
 
-    static Scanner sc = new Scanner(System.in);
-    String fileName = "Ficheiro";
-    // FALTA DESCOBRIR O NOME DO FICHEIRO
-    static SystemManager sysM = new SystemManager();
-    {
-        try {
-            sysM.loadFile(fileName);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 
     // cria um USER novo SE ainda não existe
-    private static void createUser() {
+    private static void createUser(SystemManager sysM,Scanner sc) {
         String name = sc.next();
-        System.out.println(name);
 
         if (sysM.user_exists(name)) System.out.println(CREATE_ERROR);
         else {
             sysM.create_user(name);
             System.out.println(CREATE_USER);
-            System.out.println(sysM.get_UserCount());
         }
     }
 
     // Cria um EVENTO novo SE ainda não existe
-    private static void scheduleEvent() {
+    private static void scheduleEvent(SystemManager sysM,Scanner sc) {
         // Read / Prepare
 
         String eventName = sc.next();
@@ -98,19 +83,19 @@ public class Main {
 
 
     // apaga o evento, so o proponente pode fazer
-    private static void cancelEvent () {
+    private static void cancelEvent (SystemManager sysM,Scanner sc) {
         String eventName = sc.next();
         String user = sc.next();
-        if (!(sysM.user_exists(user))) System.out.println("User not registered");
-        else if (!(sysM.event_exists(eventName))) System.out.printf("Event not found in calendar of %s .\n",user);
-        else if(!(sysM.checkProp(user,eventName))){System.out.printf("User %s did not create event %s.\n",user,eventName);}
-        else{sysM.del_event(eventName,user);
+        if (!(sysM.user_exists(user))) System.out.println("User not registered.");
+        else if (!(sysM.isInCalender(eventName,user))) System.out.printf("Event not found in calendar of %s.\n",user);
+        else if(!(sysM.isProponent(user,eventName))){System.out.printf("User %s did not create event %s.\n",user,eventName);}
+        else{sysM.del_event(eventName);
         System.out.println(CANCEL_SUCCESS);
         }
 
     }
     // mostra o calendario de um user por ordem cronologica
-    private static void showEvent () {
+    private static void showEvent (SystemManager sysM,Scanner sc) {
         String nome = sc.next();
         sc.nextLine();
         if (!(sysM.user_exists(nome))){
@@ -118,28 +103,28 @@ public class Main {
         }
         else {
             if ((sysM.get_user(nome)).get_eventCount() == 0){
-                System.out.println("User "+nome+" has no events");
+                System.out.println("User "+nome+" has no events.");
             }
             else {
                 Event[] e_temp = (sysM.get_user(nome)).get_Events();
                 for(int i=0;i<(sysM.get_user(nome)).get_eventCount();i++){
-                    System.out.println(e_temp[i].get_name()+","+e_temp[i].get_day()+","+e_temp[i].get_start()+"-"+
-                            e_temp[i].get_end()+","+e_temp[i].get_participantCount()+ "participants");
+                    System.out.println(e_temp[i].get_name()+", day "+e_temp[i].get_day()+", "+e_temp[i].get_start()+"-"+
+                            e_temp[i].get_end()+", "+e_temp[i].get_participantCount()+ " participants.");
                 }
             }
         }
     }
     // Mostra eventos por ordem de adesao
-    private static void topEvents () {
+    private static void topEvents (SystemManager sysM,Scanner sc) {
         if(sysM.get_EventCount()==0){
-            System.out.println("No events found");
+            System.out.println("No events registered.");
         }
 
         else{
             Event [] e_temp=sysM.top_events();
             for(int i=0;i<(sysM.getCountTop());i++){
-                System.out.println(e_temp[i].get_name()+","+e_temp[i].get_day()+","+e_temp[i].get_start()+"-"+
-                        e_temp[i].get_end()+","+e_temp[i].get_participantCount()+ "participants");
+                System.out.println(e_temp[i].get_name()+", day "+e_temp[i].get_day()+", "+e_temp[i].get_start()+"-"+
+                        e_temp[i].get_end()+", "+e_temp[i].get_participantCount() + " participants.");
             }
 
 
@@ -150,7 +135,7 @@ public class Main {
         System.out.println(EXITED);
     }
 
-    private static void executeCommand (){
+    private static void executeCommand (SystemManager sysM,Scanner sc) {
 
         String command;
         do {
@@ -158,31 +143,44 @@ public class Main {
             command=sc.next();
             switch (command) {
                 case CREATE_CMD -> {
-                    createUser();
+                    createUser(sysM,sc);
                     sc.nextLine();
                 }
                 case SCHEDULE_CMD -> {
 
-                    scheduleEvent();
+                    scheduleEvent(sysM,sc);
                 }
                 case CANCEL_CMD -> {
-                    cancelEvent();
+                    cancelEvent(sysM,sc);
                 }
                 case SHOW_CMD -> {
-                    showEvent();
+                    showEvent(sysM,sc);
                 }
                 case TOP_CMD -> {
-                    topEvents();
+                    topEvents(sysM,sc);
                 }
                 case EXIT_CMD -> {
                     exit();
                 }
-                default -> System.out.println(INVALID);
+                default -> {
+                    System.out.println(INVALID_CMD);
+                            sc.nextLine();// Tive que adiconar senão andava a ler a linha seguinte
+                }
             }
 
         } while (!command.equals(EXIT_CMD));
     }
     public static void main (String[]args){
-        executeCommand();
+        Scanner sc = new Scanner(System.in);
+        String fileName = sc.nextLine();//Lê o nome do ficheiro
+        SystemManager sysM = new SystemManager();
+        {
+            try {
+                sysM.loadFile(fileName);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    executeCommand(sysM,sc);
     }
 }
