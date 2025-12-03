e30f55ddfcc54dfc018766ce4acc339da42ddea9
EltonVieira98 <eltonviei1998@gmail.com>
2025-12-03 23:48:40 +0000

    Update User.java

--- src/User.java
+++ src/User.java
@@ -2,25 +2,25 @@
 public class User{
 
     // max 200 events por user
-    private int max_Events = 200;
+    private int maxEvents = 200;
     // nome tem de ser unico
     private String name;
     // array dos eventos do user
     private Event[] events;
     // nr de eventos associados ao user
-    private int event_count;
+    private int eventCount;
 
 
     // constructor
     public User(String name){
         this.name = name;
-        this.events = new Event[max_Events];
-        this.event_count = 0;
+        this.events = new Event[maxEvents];
+        this.eventCount = 0;
     }
 
 
     //getters:
-    public String get_name(){
+    public String getName(){
         return name;
     }
     public Event[] get_Events() {
@@ -29,24 +29,25 @@ public class User{
     public Event get_Events(int idx) {
         return events[idx];
     }
-    public int get_eventCount(){
-        return event_count;
+    public int getEventCount(){
+        return eventCount;
     }
 
 
     // adiciona o proximo evento no calendario
-    public void add_event(Event eventname){
-        events[event_count]= eventname;
-        event_count ++;
-        sortUserEvents();
+    public void addEvent(Event eventName){
+        events[eventCount]= eventName;
+        eventCount++;
+        if(eventCount>1){
+            sortUserEvents();
+        }
 
     }
     //Sort by selection order
     public void sortUserEvents(){
-        if(event_count>=2){
-            for(int i=0;i<event_count;i++){
+            for(int i = 0; i< eventCount; i++){
                 int minIdx = i;
-                for(int j=i+1;j<event_count;j++){
+                for(int j = i+1; j< eventCount; j++){
                     if(sortCriteria(events[minIdx],events[j])){
                         minIdx = j;
                     }
@@ -55,46 +56,45 @@ public class User{
                 events[i]=events[minIdx];
                 events[minIdx]=tmpEventName;
             }
-        }
+
     }
 
 
 
 
     public boolean sortCriteria(Event eventI,Event eventJ){
-        if(eventI.get_day()!=eventJ.get_day()){
-            return eventI.get_day()>eventJ.get_day();}
-        if(eventI.get_start()!=eventJ.get_start()){
-            return eventI.get_start()>eventJ.get_start();}
-        if(eventI.get_end()!=eventJ.get_end()){
-            return eventI.get_end()>eventJ.get_end();
+        if(eventI.getDay()!=eventJ.getDay()){
+            return eventI.getDay()>eventJ.getDay();} //Mudar
+        if(eventI.getStart()!=eventJ.getStart()){
+            return eventI.getStart()>eventJ.getStart();}
+        if(eventI.getEnd()!=eventJ.getEnd()){
+            return eventI.getEnd()>eventJ.getEnd();
         }
-        else return eventI.get_name().compareTo(eventJ.get_name())>0;
+        else return eventI.getName().compareTo(eventJ.getName())>0;
 
     }
 
 
     // remove evento no calendario e substitui pelo ultimo elemento de events.
-    public void del_event_user(Event eventname){
-        for (int i = 0; i< event_count; i++) {
-            if (events[i].equals(eventname)) {         // usar .equals??//eu não mudei nada aqui
-                events[i] = events[event_count - 1];
-                event_count--;
+    public void delEvent(Event eventName){
+        for (int i = 0; i< eventCount; i++) {
+            if (events[i].equals(eventName)) {         // usar .equals??//eu não mudei nada aqui
+                events[i] = events[eventCount - 1];
+                eventCount--;
 
             }
         }
-
-        sortUserEvents();
+    sortUserEvents();
     }
 
 
     // verificar conflitos de horario-True se houver conflito  // day: 1-5.
     public boolean conflit(int day, int inicio, int fim){
-        for (int i=0; i< event_count; i++){
+        for (int i = 0; i< eventCount; i++){
             // metodos definidos na classe Event
-            if (events[i].get_day() == day){
+            if (events[i].getDay() == day){
                 // existe conflito? ( há sobreposição? )
-                if ( (inicio < events[i].get_end()) && (fim  > events[i].get_start()) ){
+                if ( (inicio < events[i].getEnd()) && (fim  > events[i].getStart()) ){
                     return true;
                 }
             }
